3f10642737991363df25b8284e82ae45c061b56d
NetCaffe <nabhon.k@gmail.com>
2026-03-18 21:37:16 +0700

    add loading screen with auth checking

--- lib/presentation/screens/loading/loading_screen.dart
+++ lib/presentation/screens/loading/loading_screen.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class LoadingScreen extends StatelessWidget {
+import '../../../data/datasources/supabase_client.dart';
+
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthAndNavigate();
+  }
+
+  Future<void> _checkAuthAndNavigate() async {
+    // Small delay to ensure Supabase client is ready
+    await Future.delayed(const Duration(milliseconds: 3000));
+
+    // Check authentication status
+    final isAuthenticated = SupabaseInit.client.auth.currentSession != null;
+
+    if (mounted) {
+      context.go(isAuthenticated ? '/' : '/login');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
